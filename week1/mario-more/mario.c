fa7882d2cd87cb73fff5a785c70370fc1b8f2273
byan <75323963+byyan2020@users.noreply.github.com>
2022-02-13 08:13:47 +0000

    Sun, Feb 13, 2022, 8:13 AM +00:00

--- week1/mario-more/mario.c
+++ week1/mario-more/mario.c
@@ -1,7 +1,47 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    // get the size of the pyramid
+    int size;
+    do
+    {
+        size = get_int("Please input the size of the pyramid: ");
+    }
+    while (size < 1 || size > 8);
+
+    // print line 1 to line n
+    for (k = 1, k <= size, k++)
+    {
+        // print the first pyramid
+        // print space*(size-i)
+        for (int i = 1; i <= size - k; i++)
+        {
+            printf(" ");
+        }
+        // print #*i
+        for (int j = 1; j <= k; j++)
+        {
+            printf("#");
+        }
+
+        // print two spaces
+        printf("  ");
+
+        // print the second pyramid
+        for (int m = 1; m <= k; m++ )
+        {
+            printf("#");
+        }
+
+        for (int n = 1; n <= size - k; n++)
+        {
+            printf(" ");
+        }
+
+        printf("\n");
+    }
+
+
 }
