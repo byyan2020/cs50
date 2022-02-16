31e47890a8307c4e3e974ef25ee8a485b3e72cae
byan <75323963+byyan2020@users.noreply.github.com>
2022-02-16 14:38:58 +0000

    Wed, Feb 16, 2022, 2:38 PM +00:00

--- week2/readability/readability.c
+++ week2/readability/readability.c
@@ -1,10 +1,21 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int count_letters(string text)
+
 int main(void)
 {
     // getting user input
     string text = get_string("Text:");
-    printf("%s\n", text);
 
+    // count letters
+    int letters = count_letters(text);
+
+    printf("%i\n", letters);
+
+}
+
+int count_letters(string text)
+{
+    
 }
