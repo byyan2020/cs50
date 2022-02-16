15cf2210e121132f5558ff85289ed51f2de05487
byan <75323963+byyan2020@users.noreply.github.com>
2022-02-16 14:23:49 +0000

    Wed, Feb 16, 2022, 2:23 PM +00:00

--- week2/exit.c
+++ week2/exit.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main(void)
+int main(int argc, string argv[] )
 {
+    if (argc != 2)
+    {
     printf("something went wrong\n");
     return 2;
+    }
+
+    printf("hello, %s\n", argv[1]);\
+    return 0
+
 }
