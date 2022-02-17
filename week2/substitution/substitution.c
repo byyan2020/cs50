e712d6ca1f47eec076d4029704ac93f9d49ece65
byan <75323963+byyan2020@users.noreply.github.com>
2022-02-17 14:07:40 +0000

    Thu, Feb 17, 2022, 2:07 PM +00:00

--- week2/substitution/substitution.c
+++ week2/substitution/substitution.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, string argv[])
 {
@@ -14,5 +15,17 @@ int main(int argc, string argv[])
     string key = argv[1];
 
     // test if the key is valid
-    
+    int count = 0;
+    int str_length = strlen(key);
+
+    if (str_length != 26)
+    {
+        printf("Key must contain 26 charcters\n");
+        return 1;
+    }
+
+    for (int i = 0; i < str_length; i++)
+    {
+        if iskey[i]
+    }
 }
