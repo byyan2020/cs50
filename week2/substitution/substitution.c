96e55ad8dcc442124d96de3599ac06ce7fa7629e
byan <75323963+byyan2020@users.noreply.github.com>
2022-02-17 13:59:05 +0000

    Thu, Feb 17, 2022, 1:59 PM +00:00

--- week2/substitution/substitution.c
+++ week2/substitution/substitution.c
@@ -1,7 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
-
+    // accept a single command-line argument
+    if (argc != 2)
+    {
+        
+    }
 }
