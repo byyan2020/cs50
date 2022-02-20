19824eb0cf33c250614c029204d81199196555fe
byan <75323963+byyan2020@users.noreply.github.com>
2022-02-20 08:48:48 +0000

    Sun, Feb 20, 2022, 8:48 AM +00:00

--- week2/scrabble/scrabble.c
+++ week2/scrabble/scrabble.c
@@ -19,9 +19,22 @@ int main(void)
     int score2 = compute_score(word2);
 
     // TODO: Print the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!");
+    }
+    else if(score1 < score2)
+    {
+        printf("Player 2 wins!");
+    }
+    else
+    {
+        printf("Tie!");
+    }
 }
 
 int compute_score(string word)
 {
     // TODO: Compute and return score for string
+    
 }
