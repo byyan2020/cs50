c26f71285d72895be61eb250f5fa6991f753373d
byan <75323963+byyan2020@users.noreply.github.com>
2022-02-23 13:42:38 +0000

    Wed, Feb 23, 2022, 1:42 PM +00:00

--- week3/tideman/tideman.c
+++ week3/tideman/tideman.c
@@ -205,7 +205,11 @@ void sort_pairs(void)
 // Lock pairs into the candidate graph in order, without creating cycles
 void lock_pairs(void)
 {
-    
+    /*
+    pairs from 0 to n
+        if no cycle
+            locked[i][j] = true
+     */
     return;
 }
 
