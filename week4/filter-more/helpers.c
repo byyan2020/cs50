28daa055f0d704e44b7a25c1baf7635e4bfcbdb6
byan <75323963+byyan2020@users.noreply.github.com>
2022-02-27 09:16:02 +0000

    Sun, Feb 27, 2022, 9:16 AM +00:00

--- week4/filter-more/helpers.c
+++ week4/filter-more/helpers.c
@@ -88,5 +88,51 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    // get a new array
+    RGBTRIPLE copy[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            copy[i][j] = image[i][j];
+        }
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int sum_red = 0;
+            int sum_green = 0;
+            int sum_blue = 0;
+            float counter = 0;
+            for (int k = i-1; k < (i + 2); k++)
+            {
+                for (int l = j-1; l < (j + 2); l++)
+                {
+                    if (k < 0 || l < 0 || k >= width || l >= width)
+                    {
+                        continue;
+                    }
+                    sum_red += image[k][l].rgbtRed;
+                    sum_green += image[k][l].rgbtGreen;
+                    sum_blue += image[k][l].rgbtBlue;
+                    counter++;
+                }
+            }
+
+            copy[i][j].rgbtRed = round(sum_red/counter);
+            copy[i][j].rgbtGreen = round(sum_green/counter);
+            copy[i][j].rgbtBlue = round(sum_blue/counter);
+            // printf("%i, %i, %i\n", image[i][j].rgbtRed, image[i][j].rgbtGreen, image[i][j].rgbtBlue);
+        }
+    }
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j] = copy[i][j];
+        }
+    }
     return;
 }
