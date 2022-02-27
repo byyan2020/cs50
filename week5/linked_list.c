a37884f0ca520e1086b4d3ccde4aba6b7b1957db
byan <75323963+byyan2020@users.noreply.github.com>
2022-02-27 05:04:06 +0000

    Sun, Feb 27, 2022, 5:04 AM +00:00

--- week5/linked_list.c
+++ week5/linked_list.c
@@ -16,5 +16,12 @@ int main(void)
 
     // add number to list
     node *n = malloc(sizeof(node));
-    
+    if (n == NULL)
+    {
+        return 1;
+    }
+    n->number = 1;
+    n->next = NULL;
+
+    list = n;
 }
