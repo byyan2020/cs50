11c3eee9a5c0f71f132634440c65ac7b0a29c7f5
byan <75323963+byyan2020@users.noreply.github.com>
2022-02-28 16:32:45 +0000

    Mon, Feb 28, 2022, 4:32 PM +00:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -4,8 +4,13 @@
 int main(int argc, char *argv[])
 {
     // check for one command-line argument
+    if (argc != 2)
+    {
+        printf("Usage: ./recover IMAGE");
+        return 1;
+    }
 
     // check for if the forensic image can be open for reading
+    FILE *file = fopen(argv[1], "r");
 
-    
 }
