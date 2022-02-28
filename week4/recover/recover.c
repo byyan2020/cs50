dcc8c4200ae8cf743b655549ea6a70f21cc3f0fe
byan <75323963+byyan2020@users.noreply.github.com>
2022-02-28 17:40:09 +0000

    Mon, Feb 28, 2022, 5:40 PM +00:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -65,7 +65,7 @@ int main(int argc, char *argv[])
     }
 }
 
-bool isJpeg(BYTE buffer[])
+bool isJpeg(BYTE *buffer)
 {
     if (buffer[0] == 0xff & buffer[1] == 0xd8 & buffer[2] == 0xff & (buffer[3] & 0xf0) == 0xe0)
     {
