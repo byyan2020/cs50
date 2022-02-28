ebcf34f073cb2ed9e104bd44b77532a96b3309f6
byan <75323963+byyan2020@users.noreply.github.com>
2022-02-28 17:09:07 +0000

    Mon, Feb 28, 2022, 5:09 PM +00:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    BYTE *buffer[BLOCK_SIZE] = malloc(sizeof(BYTE) * 512);
+    BYTE *buffer = malloc(sizeof(BYTE) * 512);
 
     while (fread(buffer, 1, BLOCK_SIZE, file) == BLOCK_SIZE);
     // look for beginning of a JPEG
@@ -33,12 +33,11 @@ int main(int argc, char *argv[])
 
     // if is jpeg
         // open a new JPEG file
-        // sprintf(filename, "%03i.jpg", 2);
-        // FILE *img = fopen(filename, "w");
-        // write 512 byte
-    // else
-        // write 512 byte
+        sprintf(filename, "%03i.jpg", 2);
+        FILE *img = fopen(filename, "w");
 
+    // write 512 byte
+    fwrite(buffer, 1, BLOCK_SIZE, img);
 
 
         while (isJpeg)
