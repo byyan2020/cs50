21304c48fa2ce87f0c89a983061a74efda38010f
byan <75323963+byyan2020@users.noreply.github.com>
2022-03-05 17:44:33 +0000

    Sat, Mar 5, 2022, 5:44 PM +00:00

--- week6/sentimental-credit/credit.py
+++ week6/sentimental-credit/credit.py
@@ -5,13 +5,14 @@ number = str(get_int("Number: "))
 sum = 0
 length = len(number)
 for i in range(length-2, -1, -2):
-    print(i, int(number[i]))
-    sum += int(number[i]) * 2
-print(sum)
+    # print(i, int(number[i]))
+    for n in str(int(number[i]) * 2):
+        sum += int(n)
+# print(sum)
 for j in range(length-1, -1, -2):
-    print(j, int(number[j]))
+    # print(j, int(number[j]))
     sum += int(number[j])
-print(sum)
+# print(sum)
 
 print(sum)
 if sum % 10 != 0:
