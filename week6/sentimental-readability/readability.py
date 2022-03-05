e148a2cd5c3c180d7f8ad342f74b8bf9614ca280
byan <75323963+byyan2020@users.noreply.github.com>
2022-03-05 17:57:16 +0000

    Sat, Mar 5, 2022, 5:57 PM +00:00

---
+++ week6/sentimental-readability/readability.py
@@ -0,0 +1,31 @@
+from cs50 import get_string
+
+def main():
+    text = get_string("Text: ")
+    letters = count_letters(text)
+    words = count_words(text)
+    sentence = count_sentences(text)
+    L = letters / words * 100
+    S = sentences / words * 100
+    index = round(0.0588 * L - 0.296 * S - 15.8)
+
+    if index > 16:
+        print("Grade 16+")
+    elif index < 1:
+        print("Before Grade 1")
+    else:
+        print(f"Grade {index}")
+
+
+
+def count_letters(text):
+    return
+
+def count_words(text):
+    return
+
+def count_sentence(text):
+    return
+
+if __name__ == __main__:
+    main()
