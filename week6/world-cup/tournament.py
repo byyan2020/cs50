f6ad586bbfef51e418d5daa1b104d59faff0e517
byan <75323963+byyan2020@users.noreply.github.com>
2022-03-05 15:18:28 +0000

    Sat, Mar 5, 2022, 3:18 PM +00:00

--- week6/world-cup/tournament.py
+++ week6/world-cup/tournament.py
@@ -16,7 +16,7 @@ def main():
 
     teams = []
     # TODO: Read teams into memory from file
-    with open("sys.argv[1]", "r") as file:
+    with open(sys.argv[1], "r") as file:
         reader = csv.DictReader(file)
         for team in reader:
             team["rating"] = int(team["rating"])
