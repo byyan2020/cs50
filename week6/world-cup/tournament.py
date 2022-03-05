298ba8e5d089c41645b01604b18abdb6265bef90
byan <75323963+byyan2020@users.noreply.github.com>
2022-03-05 14:49:13 +0000

    Sat, Mar 5, 2022, 2:49 PM +00:00

--- week6/world-cup/tournament.py
+++ week6/world-cup/tournament.py
@@ -23,10 +23,10 @@ def main():
             teams.append(team)
 
     counts = {}
-    # TODO: Simulate N tournaments and keep track of win counts
     for team in teams:
         counts[team[team]] = team[rating]
 
+    # TODO: Simulate N tournaments and keep track of win counts
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
