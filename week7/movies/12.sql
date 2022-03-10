8a35d76d8a583ae126040b4aea5bb71508e623ce
byan <75323963+byyan2020@users.noreply.github.com>
2022-03-10 17:44:54 +0000

    Thu, Mar 10, 2022, 5:44 PM +00:00

--- week7/movies/12.sql
+++ week7/movies/12.sql
@@ -2,4 +2,5 @@ SELECT title FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON stars.person_id = people.id
 WHERE name = "Johnny Depp"
+GROUP BY title
 HAVING name = "Helena Bonham Carter";
