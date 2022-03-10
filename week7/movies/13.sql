3c590f96adde7f95ac568615286d26b8cdc1d4ea
byan <75323963+byyan2020@users.noreply.github.com>
2022-03-10 17:54:50 +0000

    Thu, Mar 10, 2022, 5:54 PM +00:00

--- week7/movies/13.sql
+++ week7/movies/13.sql
@@ -1,3 +1,8 @@
 SELECT name FROM people
 JOIN stars ON movies.id = stars.movie_id
 JOIN movie ON movie.id = stars.movie_id
+WHERE id IN (
+    SELECT person_id FROM stars WHERE movie_id IN (
+        
+    )
+    )
