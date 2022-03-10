78750387f7b05737da13b81a0092b89fc20ba3e6
byan <75323963+byyan2020@users.noreply.github.com>
2022-03-10 17:02:43 +0000

    Thu, Mar 10, 2022, 5:02 PM +00:00

--- week6/dna/dna.py
+++ week6/dna/dna.py
@@ -5,14 +5,48 @@ import sys
 def main():
 
     # TODO: Check for command-line usage
+    if len(sys.argv) != 3:
+        sys.exit("Usage: python dna.py data.csv sequence.txt")
 
     # TODO: Read database file into a variable
-    
+    database = []
+    with open(sys.argv[1], "r") as file1:
+        reader = csv.DictReader(file1)
+        for data in reader:
+            database.append(data)
+
     # TODO: Read DNA sequence file into a variable
+    with open(sys.argv[2], "r") as file2:
+        sequence = file2.read()
 
     # TODO: Find longest match of each STR in DNA sequence
+    strs = [k for k in database[0].keys()]
+    strs = strs[1:]
+    match = {}
+    for i in strs:
+        match[i] = longest_match(sequence, i)
+    # print(f"strs:{strs}" )
+    # print(f"match: {match}" )
 
     # TODO: Check database for matching profiles
+    for person in database:
+        # print(f"person: {person}"  )
+        find_person = False
+        # check if every str is match
+        flag = 0
+        for j in strs:
+            # print(j,person[j], match[j])
+            if int(person[j]) != match[j]:
+                flag = -1
+        # print(f"flag: {flag}")
+        # if match, print name
+        if flag == 0:
+            print(person["name"])
+            find_person = True
+            break
+        # else, print no match
+    if find_person == False:
+        print("No match")
 
     return
 
@@ -43,11 +77,11 @@ def longest_match(sequence, subsequence):
             # If there is a match in the substring
             if sequence[start:end] == subsequence:
                 count += 1
-            
+
             # If there is no match in the substring
             else:
                 break
-        
+
         # Update most consecutive matches found
         longest_run = max(longest_run, count)
 
