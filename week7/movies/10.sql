261b464ec0293006684f493daafec63950b1f777
byan <75323963+byyan2020@users.noreply.github.com>
2022-03-10 17:56:52 +0000

    Thu, Mar 10, 2022, 5:56 PM +00:00

--- week7/movies/10.sql
+++ week7/movies/10.sql
@@ -2,4 +2,4 @@ SELECT DISTINCT name FROM people, movies, ratings, directors
 WHERE people.id = directors.person_id
 AND directors.movie_id = movies.id
 AND ratings.movie_id = movies.id
-AND rating > 9.0;
+AND rating >= 9.0;
