7dd55cde807e3af72470fdfdfcace87a324bd0be
byan <75323963+byyan2020@users.noreply.github.com>
2022-03-12 06:03:41 +0000

    Sat, Mar 12, 2022, 6:03 AM +00:00

--- week7/movies/12.sql
+++ week7/movies/12.sql
@@ -1,9 +1,9 @@
 SELECT title FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON stars.person_id = people.id
-WHERE name = "Johnny Depp"
-AND title IN (
-SELECT title FROM movies
+WHERE people.name = "Johnny Depp"
+AND movies.title IN (
+SELECT movies.title FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON stars.person_id = people.id
 WHERE name = "Helena Bonham Carter"
--- week7/movies/13.sql
+++ week7/movies/13.sql
@@ -1,9 +1,10 @@
 SELECT DISTINCT name FROM people
 JOIN stars ON people.id = stars.person_id
 JOIN movies ON movies.id = stars.movie_id
-WHERE title IN(
-SELECT title FROM movies
+WHERE movies.id IN(
+SELECT movies.id FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON people.id = stars.person_id
-WHERE name = "Kevin Bacon"
-AND birth = 1958);
+WHERE people.name = "Kevin Bacon"
+AND people.birth = 1958)
+AND people.name != "Kevin Bacon";
