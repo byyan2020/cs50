d82359526f37e5f088496034b77942c7115a73cf
byan <75323963+byyan2020@users.noreply.github.com>
2022-03-12 07:27:10 +0000

    Sat, Mar 12, 2022, 7:27 AM +00:00

--- week7/fiftyville/log.sql
+++ week7/fiftyville/log.sql
@@ -1,7 +1,7 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
-SELECT description
-FROM crime_scene_reports
+-- GET description from crime scene reports
+SELECT description FROM crime_scene_reports
 WHERE year = 2021
 AND month = 7
 AND day = 28
@@ -9,18 +9,27 @@ And street = "Humphrey Street";
 
 --Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery.
 
--- Get transcrip from interviews
--- Ruth    | Sometime within ten minutes of the theft, I saw the thief get into a car in the bakery parking lot and drive away. If you have security footage from the bakery parking lot, you might want to look for cars that left the parking lot in that time frame.                                                          |
--- Eugene  | I don't know the thief's name, but it was someone I recognized. Earlier this morning, before I arrived at Emma's bakery, I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money.                                                                                                 |
--- Raymond | As the thief was leaving the bakery, they called someone who talked to them for less than a minute. In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow. The thief then asked the person on the other end of the phone to purchase the flight ticket. |
--- Emma    | I'm the bakery owner, and someone came in, suspiciously whispering into a phone for about half an hour. They never bought anything.
 
+-- Get transcrip from interviews
 SELECT name, transcript FROM interviews
 WHERE year = 2021
 AND month = 7
 AND day = 28
 AND transcript LIKE "%bakery%";
 
+-- Ruth    | Sometime within ten minutes of the theft, I saw the thief get into a car in the bakery parking lot and drive away. If you have security footage from the bakery parking lot, you might want to look for cars that left the parking lot in that time frame.                                                          |
+-- Eugene  | I don't know the thief's name, but it was someone I recognized. Earlier this morning, before I arrived at Emma's bakery, I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money.                                                                                                 |
+-- Raymond | As the thief was leaving the bakery, they called someone who talked to them for less than a minute. In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow. The thief then asked the person on the other end of the phone to purchase the flight ticket. |
+-- Emma    | I'm the bakery owner, and someone came in, suspiciously whispering into a phone for about half an hour. They never bought anything.
+
+
 -- check the cars that left the parking lot in that time frame
+SELECT license_plate FROM bakery_security_logs
+WHERE year = 2021
+AND month = 7
+AND day = 28
+AND hour = 10
+AND minute BETWEEN 15 AND 25;
+
 -- ATM on Leggett street, withdrawing money
 --purchase the earliest flight out of Fiftyville tomorrow
