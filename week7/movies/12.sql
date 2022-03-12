65ea8363c7618a56668cc63388e01f4a49dff865
byan <75323963+byyan2020@users.noreply.github.com>
2022-03-12 06:04:53 +0000

    Sat, Mar 12, 2022, 6:04 AM +00:00

--- week7/movies/12.sql
+++ week7/movies/12.sql
@@ -6,5 +6,5 @@ AND movies.title IN (
 SELECT movies.title FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON stars.person_id = people.id
-WHERE name = "Helena Bonham Carter"
+WHERE people.name = "Helena Bonham Carter"
 );
