9866256db4fc1a18e06e465f9c414baaa138d14a
byan <75323963+byyan2020@users.noreply.github.com>
2022-03-12 05:58:56 +0000

    Sat, Mar 12, 2022, 5:58 AM +00:00

--- week7/movies/13.sql
+++ week7/movies/13.sql
@@ -6,5 +6,4 @@ SELECT title FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON people.id = stars.person_id
 WHERE name = "Kevin Bacon"
-AND birth = 1958)
-AND name <> "Kevin Bacon";
+AND birth = 1958);
