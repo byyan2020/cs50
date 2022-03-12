fc2e9de13acf8da589b4b4b7ff99e338bfa47ef1
byan <75323963+byyan2020@users.noreply.github.com>
2022-03-12 07:16:36 +0000

    Sat, Mar 12, 2022, 7:16 AM +00:00

--- week7/fiftyville/log.sql
+++ week7/fiftyville/log.sql
@@ -11,13 +11,6 @@ And street = "Humphrey Street";
 
 SELECT name, transcript FROM interviews
 WHERE year = 2021
-AND name IN (
-    SELECT name FROM people
-    WHERE license_plate IN (
-        SELECT license_plate
-        FROM bakery_security_logs
-        WHERE year = 2021
-        AND month = 7
-        AND day = 28
-        AND hour = 10)
-);
+AND month = 7
+AND day = 28
+;
